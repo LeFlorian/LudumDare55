6892d871fa4d85c39432cf451e9b82ab4d95e5b6
Texier Florian <texier.florian1@gmail.com>
2024-04-14 10:44:03 +0200

    Fix collision of goutte

--- Assets/Scripts/CollideFloor.cs
+++ Assets/Scripts/CollideFloor.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 
 public class CollideFloor : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag != "Player")
+        if (other.gameObject.tag != "Player" && other.gameObject.tag != "Goutte")
         {
+            Rigidbody rb = GetComponent<Rigidbody>();
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -rb.velocity, out hit))
+            {
+                transform.position = hit.point;
+            }
+
+
             Destroy(GetComponent<Rigidbody>());
             Destroy(GetComponent<Collider>());
+
+
 
         }
     }
