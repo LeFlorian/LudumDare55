0224228800ef5a658966689e2e2ebd4fde57ade7
Texier Florian <texier.florian1@gmail.com>
2024-04-14 18:27:04 +0200

    Animation modifications

--- Assets/Scripts/GoutteSpawner.cs
+++ Assets/Scripts/GoutteSpawner.cs
@@ -15,15 +15,15 @@ public class GoutteSpawner : MonoBehaviour
     {
         while (Timer.instance.timerActive)
         {
-            float size = (float)(Random.value * 0.5);
-            Vector3 goutteSize = new Vector3(size, (float)0.05, size);
+            float size = Mathf.Clamp((float)(Random.value * 0.3),0.1f,0.3f);
+            Vector3 goutteSize = new Vector3(size, (float)0.03, size);
             yield return new WaitForSeconds(waitTime); 
             GameObject newGoutte = Instantiate(gouttes);
             newGoutte.transform.localScale = goutteSize;
             newGoutte.transform.position = transform.position;
             newGoutte.transform.parent = GameManager.Instance.goutteCollector.transform;
             
-            newGoutte.GetComponent<Rigidbody>().AddForce(transform.forward * (Random.value * 100) + playerRB.velocity);
+            newGoutte.GetComponent<Rigidbody>().AddForce(transform.forward * (Random.value * 100) + playerRB.velocity*2);
         }
     }
 
